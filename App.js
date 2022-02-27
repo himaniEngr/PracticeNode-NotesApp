401daa3e7e60a2d4469a782025a7e72cdb01f4b4
Himani <himani9.engg@gmail.com>
2022-02-27 15:13:23 +0530

    exported name from utils

---
+++ App.js
@@ -0,0 +1,6 @@
+firstName = require("./utils.js");
+// fs = require("fs");
+// fs.writeFileSync("notes.txt", "created existing file ");
+// fs.appendFileSync("notes.txt", "updated existing file ");
+// const name = "Andrew";
+console.log(firstName);
