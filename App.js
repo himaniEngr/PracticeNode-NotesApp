ee7c73b8421284e8c6578f00b41648cab7b6fae0
Himani <himani9.engg@gmail.com>
2022-03-02 11:48:01 +0530

    created commands using yargs

--- App.js
+++ App.js
@@ -5,14 +5,48 @@
 const chalk = require("chalk");
 console.log(chalk.red.bold("Error"));
 
-console.log(process.argv);
+// console.log(process.argv);
 
-const command = process.argv[2];
-if (command === "add") {
-  console.log("Adding note");
-} else if (command === "remove") {
-  console.log("Removing note");
-}
+const yargs = require("yargs");
+yargs.version("1.0.0");
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("adding a new note");
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  handler: function () {
+    console.log("removing a  note");
+  },
+});
+yargs.command({
+  command: "list",
+  describe: "list all notes",
+  handler: function () {
+    console.log("listing all the notes");
+  },
+});
+yargs.command({
+  command: "read",
+  describe: "Read a note",
+  handler: function () {
+    console.log("reading a  note");
+  },
+});
+
+console.log(yargs.argv);
+
+// const command = process.argv[2];
+// if (command === "add") {
+//   console.log("Adding note");
+// } else if (command === "remove") {
+//   console.log("Removing note");
+// }
 
 // firstName = require("./utils.js");
 // fs = require("fs");
